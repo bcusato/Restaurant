56d6210bbfcd0b76d3bf26a7491b1ae39ded2cc7
Brandon <brandoncusato@gmail.com>
2024-05-09 18:50:09 -0400

    changes

--- little-lemon/src/components/sections/headingPages/TestimonialCarousel.js
+++ little-lemon/src/components/sections/headingPages/TestimonialCarousel.js
@@ -1,25 +1,20 @@
-
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import TestimonialCard from './CardInfo/TestimonialCard';
 export default function CarouselPage() {
     return (
         <Carousel infiniteLoop={true} autoPlay={true} Interval={5000} showStatus={false}>
-            <TestimonialCard name="Micheal Caldwell" description="This is the best Mediterranean food that I've ever had!"/>
-            <TestimonialCard name="Alan Chen" description="My Shiba Inu, Mugi, really loved the cozy vibes and 
-                delicious food here."/>
-            <TestimonialCard name="Casey Lau" description="I've had some great Mediterranean food before, but none of them beats
-             Little Lemon in flavor and texture."/>
-            <TestimonialCard name="John Rosenblum" description="Great food, welcoming staff, cozy atmosphere. A great place to 
-                treat your kids to."/>
-            <TestimonialCard name="Jim Reynor" description="The food here really refreshed me after a late night shift
-                at the local supply depot."/>
-            <TestimonialCard name="Brian Dean" description="I came to Little Lemon after a 5 hour flight from the East Coast.
-                 The food here tasted so delicious after the trip here."/>
-            <TestimonialCard name="Tyler Tohmine" description="The food here was fire!! Everyone should try this place
+            <TestimonialCard name="Alex Wurst" description="This is the best Mediterranean food I've ever had!"/>
+            <TestimonialCard name="Jacob Walker" description="Amazing staff, well decorated and amazing food."/>
+            <TestimonialCard name="Sarah Raj" description="Very cozy and welcoming vibe, will definitiely be coming here again."/>
+            <TestimonialCard name="Kevin Lee" description="A great restaurant to bring your family to. Met the owners, very nice and down to 
+                earth people!"/>
+            <TestimonialCard name="Martin Smith" description="The variety in dishes beats any other Mediterranean restaurants in the midwest."/>
+            <TestimonialCard name="George Jackson" description="The food here was delicious! Everyone should try this place
                  out at least once if they live in Chicago."/>
-            <TestimonialCard name="Jack Hu" description="This restaurant served as a perfect dinner for me after a 
-                long night of studying. I would definitely order from here again!"/>
+            <TestimonialCard name="Emilia Goldberg" description="I came to Little Lemon after a 4 hour flight from Seatte.
+                 A perfect way to end a long day of travel!"/>
+            <TestimonialCard name="Juliette Villeneuve" description="I love the atmosphere, so relaxing and beautiful!"/>
              </Carousel>
     )
 }
